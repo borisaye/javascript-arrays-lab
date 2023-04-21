1be09b83d47a7854f70ec4bac725d0a69931e170
Borisaye <boris_aroche@hotmail.com>
2023-04-21 14:08:41 -0400

    .

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+    kittens.push(name)
+    return;
+
+}
+
+function destructivelyPrependKitten(name){
+    kittens.unshift(name)
+    return;
+
+}
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop()
+    return;
+
+}
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift()
+    return;
+
+}
+
+function appendKitten(name){
+    const newkittens = [...kittens, name];
+    return newkittens;
+
+}
+
+function prependKitten(name){
+    const newkittens = [name, ...kittens,];
+    return newkittens;
+
+}
+
+function removeLastKitten(name){
+    const newkittens = [...kittens];
+    newkittens.pop()
+    return newkittens;
+}
+
+function removeFirstKitten(name){
+    const newkittens = [...kittens];
+    newkittens.shift()
+    return newkittens;
+}
